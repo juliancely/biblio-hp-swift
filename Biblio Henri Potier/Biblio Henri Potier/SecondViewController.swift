2671f6934afc370445a372aa95f4c4257df6b6a7
Julian Cely <julian.cely-ariza@actimage.com>
2017-10-12 13:54:30 +0200

    Start project

---
+++ Biblio Henri Potier/Biblio Henri Potier/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Biblio Henri Potier
+//
+//  Created by Julian Cely on 12.10.17.
+//  Copyright © 2017 excercice. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
