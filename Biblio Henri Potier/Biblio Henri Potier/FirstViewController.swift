1e2dad22c0059119c21deac694c677e78761ea8c
Julian Cely <julian.cely-ariza@actimage.com>
2017-10-12 18:21:32 +0200

    Add Alamofire submodule and test API

--- Biblio Henri Potier/Biblio Henri Potier/FirstViewController.swift
+++ Biblio Henri Potier/Biblio Henri Potier/FirstViewController.swift
@@ -7,11 +7,28 @@
 //
 
 import UIKit
+import Alamofire
 
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        Alamofire.request("http://henri-potier.xebia.fr/books", method: .get).responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
